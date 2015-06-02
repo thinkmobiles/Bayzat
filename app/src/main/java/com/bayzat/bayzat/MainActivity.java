6f92ce0208b81b7d2cc635171640393c5ca1efa0
Michael Soyma (TM) <matanaliz93@mail.ru>
2015-06-02 16:30:57 +0300

    Initial commit

---
+++ app/src/main/java/com/bayzat/bayzat/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bayzat.bayzat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Michael on 02.06.2015.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
