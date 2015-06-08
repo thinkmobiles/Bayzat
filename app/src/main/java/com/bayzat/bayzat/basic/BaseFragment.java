4916c0777ad468bd46f6a710a635e75eea9d7ac7
Michael Soyma (TM) <matanaliz93@mail.ru>
2015-06-08 17:41:38 +0300

    Architecture design.  - Fragment controller  - Base Activity  - Base Fragment  - Action bar controller - Toolbar  - Splash activity  - Main activity

---
+++ app/src/main/java/com/bayzat/bayzat/basic/BaseFragment.java
@@ -0,0 +1,56 @@
+package com.bayzat.bayzat.basic;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Michael on 08.06.2015.
+ */
+public abstract class BaseFragment extends Fragment {
+
+    //____________________ Protected variables ___________________//
+    protected BaseActivity  mActivity;
+    protected View          inflatedView;
+
+    //_____________________ Private variables ____________________//
+    private int             fragmentResId = -1;
+
+    /**
+     * The method sets resources into fragment.
+     * @param resId - resource for inflating view in fragment
+     */
+    protected void setContentView(int resId){
+        this.fragmentResId = resId;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        mActivity = (BaseActivity) activity;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        if (fragmentResId != -1) {
+            inflatedView = inflater.inflate(fragmentResId, container, false);
+            return inflatedView;
+        }
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    /**
+     * The method finding view in parent view
+     */
+    @SuppressWarnings("unchecked")
+    protected <T extends View> T $(int resId){
+        if (inflatedView == null )
+            return null;
+        return (T) inflatedView.findViewById(resId);
+    }
+}
